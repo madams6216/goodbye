aae2bc0aeb555c3a33ba2b3e737edc2a85067194
Michael Adams <madams6216@gmail.com>
2015-08-27 09:46:34 -0400

    condensed route

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,12 @@
 class HomeController < ApplicationController
   def hello
+  	hello = params[:hello]
+  	@hello = hello
   end
 
   def goodbye
+  	goodbye = [:goodbye]
+  	@goodbye = goodbye
   	
 
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  get 'home/hello'
+  get 'hello' => 'home#hello'
 
-  get 'home/goodbye'
+  get 'goodbye'=>  'home#goodbye'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
